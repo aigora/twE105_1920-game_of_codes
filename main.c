c20daef5edf33f7cdf732cc4b640918772079d8a
cbestevez <47811560+cbestevez@users.noreply.github.com>
2020-04-20 15:10:32 +0200

    error en switch de repeticion

--- main.c
+++ main.c
@@ -38,29 +38,38 @@ int main ()
 			case 4:
 				// función referida al juego 4
 			break;
+			case 5:
+				printf("Has escogido salir del juego, hasta pronto");
+				sleep(1);
+				exit(-1);
 		}
-		printf("Genial!! Has terminado el juego, espero que te hayas divertido.\n Quieres jugar a otro juego?\t Pulsa '1'\n Prefieres salir del juego?\t Pulsa '0'");
+		sleep(5);
+		system("cls");
+		printf("Genial!! Has terminado el juego, espero que te hayas divertido.\n Quieres jugar a otro juego?\t Pulsa '1'\n Prefieres salir del juego?\t Pulsa '0'\n");
 		do //segun la opcion que escoja el jugador, el programa se repite o no
 		{
 			scanf(" %i", &repet);
 			switch(repet)
 			{
 				case 1: //si se escoge esta opcion(1), se repite la posibilidad de escoger juego
+					system("cls");
 					printf("Estupendo, a que juego prefieres jugar ahora?\n");
-					printf("1) Tres en raya\n2) Adivina el numero\n3) Heroes\n4) (el juego que pongamos aqui)\n");
+					printf("1) Tres en raya.\n2) Adivina el numero.\n3) Heroes.\n4)...\n5) Salir del programa.\n");
 					a!=5;
+					repet!=6;
 				break;
-				case 2://si se escoge esta opcion(0), se finaliza el programa
+				case 0://si se escoge esta opcion(0), se finaliza el programa
+					system("cls");
 					printf("Espero que te hayas divertido, hasta pronto\n");
 					exit(-1); //permite salir del programa
 				break;
 				default:
-					printf("No te he entendido, repite la opcion por favor\n");
+					system("cls");
+					printf("No te he entendido, repite la opcion por favor: repetir(1) o salir(0)\n");
+					a=5;
 					repet=6;
-				break;
-					
+				break;	
 			}
-		}
-		while(repet=6);
+		}while(repet=6);
 	} while (a!=5);
 }
