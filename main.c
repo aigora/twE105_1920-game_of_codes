9ee543d509a682f421ec5b0b101c6ff7eae8e452
hfranco47 <47787868+hfranco47@users.noreply.github.com>
2020-04-20 12:35:39 +0200

    Update main.c

--- main.c
+++ main.c
@@ -16,7 +16,7 @@ int main ()
 		        \n 1) Tres en raya.\
 				\n 2) Adivina el numero.\
 				\n 3) Heroes.\
-				\n 4) Preguntados.\ 
+				\n 4) Preguntados.
 			        //Preguntados: juego de responder preguntas de cultura generl sobre diversos temas.
 			        // Seguramente añadiremos más juegos
 				\n 5) Salir del programa.\n ");
